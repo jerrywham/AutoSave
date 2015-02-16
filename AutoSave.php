d7675893781358ee68d6c7b65e85f5a92eaa50d2
Cyril MAGUIRE <github@ecyseo.net>
2015-02-16 11:37:15 +0100

    Simplification of the script

--- AutoSave.php
+++ AutoSave.php
@@ -3,8 +3,8 @@
  * Plugin AutoSave
  *
  * @package	PLX
- * @version	0.4
- * @date	13/02/2015
+ * @version	0.4bis
+ * @date	16/02/2015
  * @author	Rockyhorror, Cyril MAGUIRE
  **/
  
@@ -24,26 +24,9 @@ class AutoSave extends plxPlugin {
 		parent::__construct($default_lang);
 		
 		# Déclarations des hooks
-		$this->addHook('AdminTopEndHead', 'AdminTopEndHead');
 		$this->addHook('AdminFootEndBody', 'AdminFootEndBody');
-
-		# droits pour accèder à la page config.php du plugin
-		$this->setConfigProfil(PROFIL_ADMIN, PROFIL_MANAGER);
+		$this->addHook('AdminPrepend', 'AdminPrepend');
 	}
-
-	/**
-	 * Méthode qui configure le plugin à l'activation
-	 *
-	 * @return	stdio
-	 * @author	Cyril MAGUIRE
-	 **/
-	public function onActivate() {
-		if(!is_file($this->plug['parameters.xml'])) {
-			$this->setParam('duration', 10, 'numeric');
-			$this->saveParams();
-		}
-	}
-
 
 	/**
 	 * Méthode qui charge les fichiers javascripts nécessaires
@@ -53,11 +36,10 @@ class AutoSave extends plxPlugin {
 	 **/
 	public function AdminFootEndBody() {
 		echo "\t".'<script type="text/javascript">
-				if(typeof(jQuery) === "undefined") {document.write(\'<script  type="text/javascript" src="<?php echo PLX_PLUGINS; ?>AutoSave/js/jQuery.1.8.1.min.js"><\/script>\');}
+				if(typeof(jQuery) === "undefined") {document.write(\'<script  type="text/javascript" src="'.PLX_PLUGINS.'AutoSave/js/jQuery.1.8.1.min.js"><\/script>\');}
 			</script>'."\n";
 		echo "\t".'<script type="text/javascript" src="'.PLX_PLUGINS.'AutoSave/js/sisyphus/sisyphus.min.js"></script>'."\n";
 		echo "\t".'<script type="text/javascript" src="'.PLX_PLUGINS.'AutoSave/js/AutoSave.js"></script>'."\n";
-		echo "\t".'<script type="text/javascript">AutoSave('.$this->getParam('duration').',$);</script>'."\n";
 	}
 }
 ?>
